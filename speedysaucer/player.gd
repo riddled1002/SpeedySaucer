8c04f29a46ded1bc63950125da10234e69cb5555
riddled1002 <chrisandmelissa31643@outlook.com>
2024-11-07 05:19:33 -0500

    More practice with functions.

--- speedysaucer/player.gd
+++ speedysaucer/player.gd
@@ -3,16 +3,14 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	test()
+	printDoubled(3)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 	
-func test() -> void:
-	#print("I made a function!")
-	var calc = 2 + 3
-	print(calc)
+func printDoubled(input_number):
+	print(input_number * 2)
 	
 	
